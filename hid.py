1fa05ed767aeaa144830e15bba1fa7d6eca4bbb0
Pharaok <farouk.tamer03@gmail.com>
2022-03-31 03:53:35 +0200

    Create KeyCode and Modifier classes, implement ModifierBitField class and press function in KeyArray.

--- hid.py
+++ hid.py
@@ -5,31 +5,133 @@ from collections import defaultdict
 
 
 class Modifier():
-    NONE = 0x00
-    LEFT_CONTROL = 0x01
-    LEFT_SHIFT = 0x02
-    LEFT_ALT = 0x04
-    LEFT_GUI = 0x08
-    RIGHT_CONTROL = 0x10
-    RIGHT_SHIFT = 0x20
-    RIGHT_ALT = 0x40
-    RIGHT_GUI = 0x80
+    def __init__(self, x: Modifier | int = 0) -> None:
+        self._mod = x._mod if isinstance(x, Modifier) else x
 
-    def __class_getitem__(cls, key: str):
+    def __str__(self) -> str:
+        return str(self._mod)
+
+    def __format__(self, x: str) -> str:
+        return format(self._mod, x)
+
+    def __bool__(self):
+        return bool(self._mod)
+
+    def __invert__(self) -> Modifier:
+        return Modifier(~self._mod)
+
+    def __or__(self, x: Modifier | int) -> Modifier:
+        n = x._mod if isinstance(x, Modifier) else x
+        return Modifier(self._mod | n)
+
+    def __ror__(self, x: Modifier | int) -> Modifier:
+        return self | x
+
+    def __and__(self, x: Modifier | int) -> Modifier:
+        n = x._mod if isinstance(x, Modifier) else x
+        return Modifier(self._mod & n)
+
+    def __rand__(self, x: Modifier | int) -> Modifier:
+        return self & x
+
+    def __xor__(self, x: Modifier | int) -> Modifier:
+        return Modifier(self._mod ^ (x._mod if isinstance(x, Modifier) else x))
+
+    def __rxor__(self, x: Modifier | int) -> Modifier:
+        return self ^ x
+
+    def __rshift__(self, x: int) -> Modifier:
+        return Modifier(self._mod >> x)
+
+    def __lshift__(self, x: int) -> Modifier:
+        return Modifier(self._mod << x)
+
+
+class Modifiers():
+    NONE = Modifier(0x00)
+    LEFT_CONTROL = Modifier(0x01)
+    LEFT_SHIFT = Modifier(0x02)
+    LEFT_ALT = Modifier(0x04)
+    LEFT_GUI = Modifier(0x08)
+    RIGHT_CONTROL = Modifier(0x10)
+    RIGHT_SHIFT = Modifier(0x20)
+    RIGHT_ALT = Modifier(0x40)
+    RIGHT_GUI = Modifier(0x80)
+
+    def __class_getitem__(cls, key: str) -> Modifier:
         keys = defaultdict(lambda: cls.NONE)
+        keys |= {k: v for k, v in cls.__dict__.items()}
         keys |= {chr(ord('A') + i): cls.LEFT_SHIFT for i in range(26)}
         keys |= {c: cls.LEFT_SHIFT for c in ['!@#$%^&*()']}
         keys |= {c: cls.LEFT_SHIFT for c in ['_+{}|']}
         keys |= {c: cls.LEFT_SHIFT for c in [':"~<>?']}
 
-        return keys[key]
+        return Modifier(keys[key])
+
+
+class ModifierBitField(Modifier):
+    def press(self, *mods: Modifier | int) -> None:
+        for mod in mods:
+            if self & mod:
+                raise ValueError(
+                    f"Cannot release already released modifier: {mod}")
+            self |= mod
+
+    def release(self, *mods: Modifier | int) -> None:
+        for mod in mods:
+            if self & ~mod:
+                raise ValueError(
+                    f"Cannot release already released modifier: {mod}")
+            self &= ~mod
+
+
+class KeyCode():
+    def __init__(self, x: KeyCode | int = 0) -> None:
+        self._key_code = x._key_code if isinstance(x, KeyCode) else x
+
+    def __str__(self) -> str:
+        return str(self._key_code)
+
+    def __format__(self, x: str) -> str:
+        return format(self._key_code, x)
+
+
+class KeyArray():
+    def __init__(self, keys: Optional[KeyArray | Sequence[KeyCode]] = None) -> None:
+        if keys is None:
+            keys = []
+        elif isinstance(keys, KeyArray):
+            keys = keys._keys[:keys._i]
+        self._keys = [KeyCode() for _ in range(6)]
+        self._i = 0
+        self.press(*keys)
+
+    def press(self, *keys: KeyCode) -> None:
+        for key in keys:
+            if key in self._keys:
+                raise ValueError(
+                    f"Cannot press already pressed key: 0x{key:02X}")
+            self._keys[self._i] = key
+            self._i += 1
+
+    def release(self, *keys: KeyCode) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return f"[{', '.join([str(k) for k in self._keys])}]"
+
+    def __format__(self, x: str) -> str:
+        return format(self._keys, x)
 
 
 class KeyCodes():
-    def __class_getitem__(cls, key: str):
+    def __class_getitem__(cls, key: KeyCode | str):
+        if isinstance(key, KeyCode):
+            return key
+
         keys = {k: v for k, v in cls.__dict__.items()
                 if not k.startswith('_')}
-        keys |= {' ': 0x2C}
+        keys |= {None: 0x00, ' ': 0x2C}
         keys |= {chr(ord('a') + i): 0x04 + i for i in range(26)}
         keys |= {chr(ord('A') + i): 0x04 + i for i in range(26)}
         keys |= {c: 0x1E + i for i, c in enumerate('1234567890')}
@@ -39,90 +141,74 @@ class KeyCodes():
         keys |= {c: 0x33 + i for i, c in enumerate(";'`,./")}
         keys |= {c: 0x33 + i for i, c in enumerate(':"~<>?')}
 
-        return keys[key]
+        return KeyCode(keys[key])
 
-    NONE = 0x00
-    ERROR_ROLL_OVER = 0x01
-    POST_FAIL = 0x02
-    ERROR_UNDEFINED = 0x03
-    ENTER = 0x28
-    ESCAPE = 0x29
-    BACKSPACE = 0x2A
-    SPACEBAR = 0x2C
-    CAPS_LOCK = 0x39
-    F1 = 0x3A
-    F2 = 0x3B
-    F3 = 0x3C
-    F4 = 0x3D
-    F5 = 0x3E
-    F6 = 0x3F
-    F7 = 0x40
-    F8 = 0x41
-    F9 = 0x42
-    F10 = 0x43
-    F11 = 0x44
-    F12 = 0x45
-    PRINT_SCREEN = 0x46
-    SCROLL_LOCK = 0x47
-    PAUSE = 0x48
-    INSERT = 0x49
-    HOME = 0x4A
-    PAGE_UP = 0x4B
-    DELETE = 0x4C
-    END = 0x4D
-    PAGE_DOWN = 0x4E
-    RIGHT_ARROW = 0x4F
-    LEFT_ARROW = 0x50
-    DOWN_ARROW = 0x51
-    UP_ARROW = 0x52
-    NUM_LOCK = 0x53
-    NUMPAD_DIV = 0x54
-    NUMPAD_MUL = 0x55
-    NUMPAD_SUB = 0x56
-    NUMPAD_ADD = 0x57
-    NUMPAD_ENTER = 0x58
-    NUMPAD_1 = 0x59
-    NUMPAD_END = 0x59
-    NUMPAD_2 = 0x5A
-    NUMPAD_DOWN_ARROW = 0x5A
-    NUMPAD_3 = 0x5B
-    NUMPAD_PAGE_DOWN = 0x5B
-    NUMPAD_4 = 0x5C
-    NUMPAD_LEFT_ARROW = 0x5C
-    NUMPAD_5 = 0x5D
-    NUMPAD_6 = 0x5E
-    NUMPAD_RIGHT_ARROW = 0x5E
-    NUMPAD_7 = 0x5F
-    NUMPAD_HOME = 0x5F
-    NUMPAD_8 = 0x60
-    NUMPAD_UP_ARROW = 0x60
-    NUMPAD_9 = 0x61
-    NUMPAD_PAGE_UP = 0x61
-    NUMPAD_0 = 0x62
-    NUMPAD_INSERT = 0x62
-    NUMPAD_PERIOD = 0x63
-    NUMPAD_DELETE = 0x63
-    APPLICATION = 0x65
-
-
-class KeyList():
-    def __init__(self, keys: Sequence[KeyCodes] = [], /) -> None:
-        self._keys = [0] * 6
-        for i in range(len(keys)):
-            self._keys[i] = keys[i]
-        self._keys = keys
-
-    def press(self, key: int) -> None:
-        self
+    NONE = KeyCode(0x00)
+    ERROR_ROLL_OVER = KeyCode(0x01)
+    POST_FAIL = KeyCode(0x02)
+    ERROR_UNDEFINED = KeyCode(0x03)
+    ENTER = KeyCode(0x28)
+    ESCAPE = KeyCode(0x29)
+    BACKSPACE = KeyCode(0x2A)
+    SPACEBAR = KeyCode(0x2C)
+    CAPS_LOCK = KeyCode(0x39)
+    F1 = KeyCode(0x3A)
+    F2 = KeyCode(0x3B)
+    F3 = KeyCode(0x3C)
+    F4 = KeyCode(0x3D)
+    F5 = KeyCode(0x3E)
+    F6 = KeyCode(0x3F)
+    F7 = KeyCode(0x40)
+    F8 = KeyCode(0x41)
+    F9 = KeyCode(0x42)
+    F10 = KeyCode(0x43)
+    F11 = KeyCode(0x44)
+    F12 = KeyCode(0x45)
+    PRINT_SCREEN = KeyCode(0x46)
+    SCROLL_LOCK = KeyCode(0x47)
+    PAUSE = KeyCode(0x48)
+    INSERT = KeyCode(0x49)
+    HOME = KeyCode(0x4A)
+    PAGE_UP = KeyCode(0x4B)
+    DELETE = KeyCode(0x4C)
+    END = KeyCode(0x4D)
+    PAGE_DOWN = KeyCode(0x4E)
+    RIGHT_ARROW = KeyCode(0x4F)
+    LEFT_ARROW = KeyCode(0x50)
+    DOWN_ARROW = KeyCode(0x51)
+    UP_ARROW = KeyCode(0x52)
+    NUM_LOCK = KeyCode(0x53)
+    NUMPAD_DIV = KeyCode(0x54)
+    NUMPAD_MUL = KeyCode(0x55)
+    NUMPAD_SUB = KeyCode(0x56)
+    NUMPAD_ADD = KeyCode(0x57)
+    NUMPAD_ENTER = KeyCode(0x58)
+    NUMPAD_1 = KeyCode(0x59)
+    NUMPAD_END = KeyCode(0x59)
+    NUMPAD_2 = KeyCode(0x5A)
+    NUMPAD_DOWN_ARROW = KeyCode(0x5A)
+    NUMPAD_3 = KeyCode(0x5B)
+    NUMPAD_PAGE_DOWN = KeyCode(0x5B)
+    NUMPAD_4 = KeyCode(0x5C)
+    NUMPAD_LEFT_ARROW = KeyCode(0x5C)
+    NUMPAD_5 = KeyCode(0x5D)
+    NUMPAD_6 = KeyCode(0x5E)
+    NUMPAD_RIGHT_ARROW = KeyCode(0x5E)
+    NUMPAD_7 = KeyCode(0x5F)
+    NUMPAD_HOME = KeyCode(0x5F)
+    NUMPAD_8 = KeyCode(0x60)
+    NUMPAD_UP_ARROW = KeyCode(0x60)
+    NUMPAD_9 = KeyCode(0x61)
+    NUMPAD_PAGE_UP = KeyCode(0x61)
+    NUMPAD_0 = KeyCode(0x62)
+    NUMPAD_INSERT = KeyCode(0x62)
+    NUMPAD_PERIOD = KeyCode(0x63)
+    NUMPAD_DELETE = KeyCode(0x63)
+    APPLICATION = KeyCode(0x65)
 
 
 class KeyboardReport():
-    def __init__(self, x: Optional[KeyboardReport] = None, /) -> None:
-        self.modifiers = 0
-        self.keys = []
-        if x:
-            self.modifiers = x.modifiers
-            self.keys = x.keys
+    pass
 
 
 class Keyboard(KeyboardReport):
