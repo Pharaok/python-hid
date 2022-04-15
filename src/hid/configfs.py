ecaf0491ce99743f90b12f4657f3541713cab4cf
Pharaok <farouk.tamer03@gmail.com>
2022-04-15 23:40:12 +0200

    Completely rewrite Directory class, class now directly reads/writes to/from filesystem without dictionary model of the filesystem.

--- src/hid/configfs.py
+++ src/hid/configfs.py
@@ -2,110 +2,74 @@ from __future__ import annotations
 
 import os
 import shutil
-from collections import defaultdict
-from collections.abc import Iterator, Mapping, MutableMapping
+from collections.abc import Iterator, MutableMapping
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Mapping, Optional, Union
+
+
+_KT = str
+_VT = Union['Directory', 'SymLink', str, bytes]
 
 
 @dataclass
 class SymLink:
-    dest: str
+    src: str
 
 
-_KT = str
-_VT = Union['_FileTreeNode', str, SymLink]
+@dataclass
+class Directory(MutableMapping[_KT, _VT]):
+    path: str
 
-
-class _FileTreeNode(MutableMapping[_KT, _VT]):
-    def _factory(self) -> _FileTreeNode:
-        return _FileTreeNode(self)
-
-    def __init__(self, parent: Optional[_FileTreeNode] = None,
-                 m: Optional[Mapping[_KT, _VT]] = None,
-                 k: Optional[_KT] = None
-                 ) -> None:
-        if parent is not None and k is not None:
-            # Modifies parent in-place, since it is a MutableMapping
-            # because the parent needs to have a refernce to the
-            # child node during initialization in case we need
-            # to traverse back to the root node.
-            parent._dict[k] = self
-
-        self._dict: defaultdict[_KT, _VT] = defaultdict(self._factory)
-        self._parent = parent
-
+    def __init__(self, path, m: Optional[Mapping[_KT, _VT]] = None) -> None:
         if m is None:
             m = {}
-        for k, v in m.items():
-            self[k] = v
+        self.path = path
+        os.makedirs(self.path, exist_ok=True)
 
     def __getitem__(self, k: _KT) -> _VT:
-        return self._dict.__getitem__(k)
+        p = self.path + os.sep + k
+        if os.path.isfile(p):
+            try:
+                with open(p, f'rt') as f:
+                    return f.read()
+            except UnicodeDecodeError:
+                with open(p, f'rb') as f:
+                    return f.read()
+        return Directory(p)
 
     def __setitem__(self, k: _KT, v: _VT) -> None:
-        if isinstance(v, Mapping):
-            _FileTreeNode(self, v, k)
-        elif isinstance(v, str | SymLink):
-            self._dict[k] = v
-            self._bubble_write(k, v)
+        p = self.path + os.sep + k
+
+        if not self.path.startswith(os.path.abspath(p)):
+            raise ValueError
+        if os.path.exists(p):
+            del self[k]
+
+        if isinstance(v, SymLink):
+            os.symlink(v.src, p)
+        elif isinstance(v, Mapping):
+            d = Directory(p)
+            for kk, vv in v.items():
+                d[kk] = vv
         else:
-            raise TypeError
+            os.makedirs(os.path.dirname(p), exist_ok=True)
+            m = 't' if isinstance(v, str) else 'b'
+            with open(p, f'w{m}') as f:
+                f.write(v)
 
     def __delitem__(self, k: _KT) -> None:
-        return self._dict.__delitem__(k)
+        p = self.path + os.sep + k
+        if os.path.islink(p) or os.path.isfile(p):
+            os.remove(p)
+        elif os.path.isdir(p):
+            shutil.rmtree(p)
 
-    def __iter__(self) -> Iterator[_KT]:
-        return self._dict.__iter__()
+    def __iter__(self) -> Iterator[str]:
+        return iter(os.listdir(self.path))
 
     def __len__(self) -> int:
-        return self._dict.__len__()
+        return len(os.listdir(self.path))
 
-    def clear(self) -> None:
-        super().clear()
-        self._bubble_delete("")
-
-    def setdefault(self, k: _KT, v: Optional[_VT] = None) -> _VT:
-        raise NotImplementedError
-
-    def _bubble_write(self, path: str, _v: str | SymLink) -> None:
-        # Bubble a value with its path to the root dict
-        if self._parent is None:
-            raise NotImplemented
-        for k, v in self._parent._dict.items():
-            if v is self:
-                self._parent._bubble_write(f"{k}/{path}", _v)
-
-    def _bubble_delete(self, path: str) -> None:
-        # Bubble a path to the root dict
-        if self._parent is None:
-            raise NotImplemented
-        for k, v in self._parent._dict.items():
-            if v is self:
-                self._parent._bubble_delete(f"{k}/{path}")
-
-
-class FileTree(_FileTreeNode):
-    def __init__(self, path: str, m: Optional[Mapping[_KT, _VT]] = None) -> None:
-        self.path = path
-        super().__init__(None, m)
-
-    def close(self) -> None:
-        print("del", self.path)
-        shutil.rmtree(self.path)
-
-    def _bubble_write(self, path: str, _v: str | SymLink):
-        p = f"{self.path}/{path}"
-        os.makedirs(os.path.dirname(f"{self.path}/{path}"), exist_ok=True)
-        if isinstance(_v, SymLink):
-            os.symlink(p, _v.dest)
-        elif isinstance(_v, str):
-            with open(p, "w") as f:
-                f.write(_v)
-
-    def _bubble_delete(self, path: str):
-        p = f"{self.path}/{path}"
-        if os.path.isdir(p):
-            shutil.rmtree(f"{self.path}/{path}")
-        elif os.path.isfile(p):
-            os.remove(p)
+    def __str__(self) -> str:
+        with os.popen(f"tree --noreport {self.path}", "r") as p:
+            return p.read()
